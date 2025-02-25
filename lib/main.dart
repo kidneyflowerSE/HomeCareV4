4d949a91b4273686813739ae5792cefd612cbd69
LTA207 <trongc71@gmaile.com>
2025-02-25 10:25:17 +0700

    fix order history, update policy

--- lib/main.dart
+++ lib/main.dart
@@ -38,32 +38,32 @@ import 'package:foodapp/themes/theme_provider.dart';
 
 import 'data/repository/repository.dart';
 
-void main() => runApp(
-      ChangeNotifierProvider(
-        create: (context) => ThemeProvider(),
-        child: const MyApp(),
-      ),
-    );
+// void main() => runApp(
+//       ChangeNotifierProvider(
+//         create: (context) => ThemeProvider(),
+//         child: const MyApp(),
+//       ),
+//     );
 
-// Future<void> main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   var repository = DefaultRepository();
-//   // try {
-//   //   debugPrint('Sending message...');
-//   //   await repository.sendMessage('0795335321');
-//   //   debugPrint('Message sent!');
-//   // } catch (e) {
-//   //   debugPrint('Error in main: $e');
-//   // }
-//   // var customers = await repository.loadMessage(Message(phone: '0795335321'));
-//
-//   var customers = await repository.loadCustomer();
-//   if(customers != null){
-//     for(var customer in customers){
-//       debugPrint(customer.toString());
-//     }
-//   }
-// }
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  var repository = DefaultRepository();
+  // try {
+  //   debugPrint('Sending message...');
+  //   await repository.sendMessage('0795335321');
+  //   debugPrint('Message sent!');
+  // } catch (e) {
+  //   debugPrint('Error in main: $e');
+  // }
+  // var customers = await repository.loadMessage(Message(phone: '0795335321'));
+
+  var customers = await repository.loadCleanerData();
+  if(customers != null){
+    for(var customer in customers){
+      debugPrint(customer.toString());
+    }
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
